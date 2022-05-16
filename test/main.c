0c8e04698ad857390f90f4f64d865ed2f3e79ed0
henok assefa <henicodes@gmail.com>
2022-05-16 13:29:42 -0700

    task 0- att -2

--- test/main.c
+++ test/main.c
@@ -1,7 +1,6 @@
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main - Entry point
  *
@@ -11,7 +10,7 @@ int main(void)
 {
 	int len;
 	int len2;
-	unsigned int ui;
+        unsigned int ui;
 	void *addr;
 
 	len = _printf("Let's try to printf a simple sentence.\n");
@@ -39,5 +38,6 @@ int main(void)
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
 	_printf("Unknown:[%r]\n");
+	printf("Unknown:[%r]\n");
 	return (0);
 }
